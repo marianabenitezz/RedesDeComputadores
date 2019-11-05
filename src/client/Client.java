25587eb31029a3262339a9eb1d776ef723e115a9
Gabriel Cansanção <gabrielkansan@gmail.com>
2019-11-05 14:21:00 -0400

    EnviarArquivo, criar diretorio, listar diretorios init

---
+++ src/client/Client.java
@@ -0,0 +1,267 @@
+package client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import util.Arquivo;
+import util.Status;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Client {
+	static Socket cliente;
+
+	public static void plot() throws UnknownHostException, IOException {
+		Scanner tc = new Scanner(System.in);
+		System.out.println("Estabelecendo conexão");
+		DataOutputStream dos;
+		DataInputStream in;
+		System.out.println("Conexão estabelecida");
+		System.out.println("Digite \"1\" para listar o diretorio do servidor.");
+		System.out.println("Digite \"2\" para criar um novo diretorio no servidor.");
+		System.out.println("Digite \"3\" para apagar algum diretorio do servidor.");
+		System.out.println("Digite \"4\" para enviar algum arquivo para o servidor.");
+		System.out.println("Digite \"5\" para apagar algum arquivo do servidor.");
+		System.out.println("Digite \"6\" para finalizar.");
+		try {
+			switch (tc.nextInt()) {
+			case 1:
+				cliente = new Socket("localhost", 1125);
+				System.out.println("O diretorio do padrão do servidor fica no seguinte caminho: \"c:/servidor\"");
+				System.out.println("Enviando requisição de listagem para o servidor!\n");
+
+				 dos = new DataOutputStream(cliente.getOutputStream());
+				dos.writeUTF("listar");
+				dos.flush();
+
+				in = new DataInputStream(cliente.getInputStream());
+				String listagemDiretorios = in.readUTF();
+				System.out.println(listagemDiretorios);
+				cliente.close();
+
+				plot();
+			case 2:
+				cliente = new Socket("localhost", 1125);
+				System.out.println("O diretorio padrão do servidor fica no seguinte caminho: \"c:/servidor\"");
+				System.out.println("Todos os diretorios criados serão criados dentro da pasta servidor");
+
+				System.out.print("Por gentileza, escreva o nome do novo diretorio: ");
+				String nomePasta = tc.next();
+
+				System.out.println("Enviando requisição para criar diretorio no servidor!\n");
+
+
+				dos = new DataOutputStream(cliente.getOutputStream());
+				dos.writeUTF("criarDir");
+				dos.flush();
+				dos = new DataOutputStream(cliente.getOutputStream());
+				dos.writeUTF(nomePasta);
+				dos.flush();
+
+				in = new DataInputStream(cliente.getInputStream());
+				String retorno = in.readUTF();
+				System.out.println(retorno);
+
+				plot();
+			case 3:
+				System.out.println("O diretorio do padrão do servidor fica no seguinte caminho: \"c:/servidor\"");
+				System.out.println("O diretorio que será apagado deve estar na pasta servidor");
+				
+				System.out.println("Por gentileza, digite o nome do diretorio que deseja ser deletado:");
+				String diretorioParaDelecao = tc.next();
+				
+
+				cliente = new Socket("localhost", 1125);
+				System.out.println("Enviando requisição para deletar diretorios no servidor!\n");
+				dos = new DataOutputStream(cliente.getOutputStream());
+				dos.writeUTF("deletarDiretorio");
+				dos.flush();
+				System.out.println("Enviando nome do diretorio..");
+
+				dos = new DataOutputStream(cliente.getOutputStream());
+				dos.writeUTF(diretorioParaDelecao);
+				dos.flush();
+				System.out.println("Resposta do servidor");
+				in = new DataInputStream(cliente.getInputStream());
+				 retorno = in.readUTF();
+				System.out.println(retorno);
+				cliente.close();
+				plot();
+
+			case 4:
+				System.out.println("Digite o caminho onde se encontra o arquivo, ex.: c:/cliente/lab00.pdf");
+				String caminho = tc.next();
+
+				System.out.println("O diretorio do padrão do servidor fica no seguinte caminho: \"c:/servidor\"");
+				System.out.println("Digite um novo diretorio ou digite \"c\" para continuar no diretorio padrao");
+				String dir = tc.next();
+
+				if (dir.equalsIgnoreCase("c"))
+					dir = "c:/servidor/";
+
+				File f;
+				FileInputStream file;
+
+				if (caminho.equals("c")) {
+					f = new File("c:/cliente/lab00.pdf");
+					file = new FileInputStream("c:/cliente/lab00.pdf");
+				} else {
+					f = new File(caminho);
+					file = new FileInputStream(caminho);
+				}
+
+				if (caminho.equalsIgnoreCase("c"))
+					caminho = "c:/servidor/";
+
+				cliente = new Socket("localhost", 1125);
+				dos = new DataOutputStream(cliente.getOutputStream());
+				dos.writeUTF("enviarArquivo");
+				dos.flush();
+
+				dos = new DataOutputStream(cliente.getOutputStream());
+				System.out.println(f.getName());
+				String var = f.getName();
+				dos.writeUTF(var);
+				dos.flush();
+
+				dos = new DataOutputStream(cliente.getOutputStream());
+				dos.writeUTF(dir);
+				dos.flush();
+				ObjectOutputStream output = new ObjectOutputStream(cliente.getOutputStream());
+
+				byte[] buffer = new byte[4096];
+				System.out.println("Enviando arquivo...");
+				while (true) {
+					int tamanho = file.read(buffer);
+					if (tamanho == -1)
+						break;
+					output.write(buffer, 0, tamanho);
+				}
+
+				System.out.println("Arquivo enviado.");
+				output.close();
+				cliente.close();
+
+				plot();
+
+			case 5:
+				System.out.println("O diretorio do padrão do servidor fica no seguinte caminho: \"c:/servidor\"");
+				System.out.println("O arquivo que será apagado deve estar na pasta servidor");
+				
+				
+				System.out.println("Por gentileza, digite o nome do diretorio que deseja ser deletado:");
+				String arquivoParaDelecao = tc.next();
+				
+				
+				cliente = new Socket("localhost", 1125);
+				System.out.println("Enviando requisição para deletar arquivos no servidor!\n");
+				dos = new DataOutputStream(cliente.getOutputStream());
+				dos.writeUTF("deleteArquivo");
+				dos.flush();
+				System.out.println("Enviando nome do arquivo..");
+
+				dos = new DataOutputStream(cliente.getOutputStream());
+				dos.writeUTF(arquivoParaDelecao);
+				dos.flush();
+				
+				in = new DataInputStream(cliente.getInputStream());
+				 retorno = in.readUTF();
+				System.out.println(retorno);
+				cliente.close();
+				plot();
+
+			case 6:
+				System.out.println("Programa finalizado com sucesso.");
+				System.exit(0);
+
+			default:
+				System.out.println("Numero invalido, digite novamente");
+				plot();
+
+			}
+
+		} catch (InputMismatchException inputException) {
+			System.err.println("Entrada invalida:");
+			Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, inputException);
+			System.out.print("Aperte \"Enter\" para continuar.");
+			tc.nextLine();
+			tc.nextLine().matches("\n");
+			plot();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		try {
+
+			plot();
+//			// cria conexão entre cliente e servidor
+//
+//			// cria streams I/O
+//			ObjectOutputStream output = new ObjectOutputStream(cliente.getOutputStream());
+//
+//			System.out.println("Digite o caminho do arquivo, ex.: c:/cliente/lab00.pdf");
+//			String caminho = tc.next();
+////        	File f = new File("c:/cliente/lab00.pdf");
+////        	f.getName();
+//			FileInputStream file;
+//
+//			if (caminho.equals("c"))
+//				file = new FileInputStream("c:/cliente/lab00.pdf");
+//			else
+//				file = new FileInputStream(caminho);
+//
+//			byte[] buffer = new byte[4096];
+//			System.out.println("Enviando arquivo...");
+//			while (true) {
+//				int tamanho = file.read(buffer);
+//				if (tamanho == -1)
+//					break;
+//				output.write(buffer, 0, tamanho);
+//			}
+
+//        	
+//        	
+//        	Arquivo arq = new Arquivo("Hello");
+//        	arq.setStatus(Status.SOLICITACAO);
+//        	arq.setParam("nome", "Eduardo");
+//        	arq.setParam("sobrenome", "Silva");
+//        	
+//        	output.writeObject(arq);
+//        	output.flush(); //envio do final da mensagem
+//        	
+//        	System.out.println("Mensagem "+arq+" enviada!");
+//        	
+//        	arq =(Arquivo) input.readObject();
+//        	
+//        	System.out.println("Resposta "+arq);
+//        	if(arq.getStatus() == Status.OK) {
+//        		String resposta = (String) arq.getParam("mensagem"); /**/
+//        		System.out.println("Mensagem: \n"+resposta);
+//        	}else {
+//        		System.out.println("Erro: "+ arq.getStatus());
+//        	}
+//        	
+//        	input.close();
+//        	output.close();
+//        	cliente.close();
+//        	
+
+		} catch (IOException e) {
+			System.out.println("Erro no cliente " + e);
+			Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, e);
+		}
+
+	}
+}
---
+++ src/server/Server.java
@@ -0,0 +1,205 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import util.Arquivo;
+import util.Status;
+
+public class Server {
+	private ServerSocket sever;
+	private static String caminhoDir = "c:/servidor";
+
+	private void criarServerSocket(int porta) throws IOException {
+		sever = new ServerSocket(porta);
+	}
+
+	private Socket esperaConexao() throws IOException {
+		Socket socket = sever.accept();
+		return socket;
+	}
+
+	public static String visualizarArquivos() throws IOException {
+		StringBuilder b = new StringBuilder();
+		b.append("");
+		Path dir = Paths.get(caminhoDir);
+		Files.walk(dir).forEach(Files -> {
+			b.append(Files.toString() + "\n");
+		});
+//		String a="";
+//		File file = new File(diretorio);
+//		File afile[] = file.listFiles();
+//		int i = 0;
+//		for (int j = afile.length; i < j; i++) {
+//			File arquivos = afile[i];
+//			a+=arquivos.getName()+"\n";
+//		}
+		return b.toString();
+	}
+
+	public static void deleteArquivos(File listaDelecao) {
+		if (listaDelecao.isFile()) {
+			listaDelecao.delete();
+		} else {
+			File files[] = listaDelecao.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				deleteArquivos(files[i]);
+			}
+		}
+	}
+
+	private void fechaSocket(Socket s) throws IOException {
+		s.close();
+	}
+
+	public static void main(String[] args) {
+		
+		File diretorio = new File(caminhoDir);
+		if(!diretorio.exists())
+		diretorio.mkdir();
+		try {
+
+			while (true) {
+
+				ServerSocket server = new ServerSocket(1125);
+				System.out.println("Aguardadndo conexao");
+				System.out.println("Servidor aguardando...");
+				DataOutputStream dos;
+
+				Socket ser = server.accept();
+
+				DataInputStream in = new DataInputStream(ser.getInputStream());
+				String mensagem = in.readUTF();
+
+				switch (mensagem) {
+				case "listar":
+
+					System.out.println("Requisição de listagem dos diretorios do servidor:");
+					dos = new DataOutputStream(ser.getOutputStream());
+					String lista = visualizarArquivos();
+					dos.writeUTF(lista);
+					dos.flush();
+					server.close();
+
+					break;
+				case "criarDir":
+
+					System.out.println("Requisição para criar diretorio no servidor!");
+					in = new DataInputStream(ser.getInputStream());
+					String nomeDiretorio = in.readUTF();
+
+					// criar diretorio
+					 diretorio = new File(caminhoDir+"/" + nomeDiretorio);
+					diretorio.mkdir();
+
+					// printa nova lista de diretorios
+					dos = new DataOutputStream(ser.getOutputStream());
+					String novaLista = visualizarArquivos();
+					dos.writeUTF(novaLista);
+
+					dos.flush();
+					server.close();
+
+					break;
+
+				case "deleteArquivo":
+					System.out.println("recebendo diretorio do arquivo para deleção...");
+					System.out.println();
+					in = new DataInputStream(ser.getInputStream());
+					String nomeDir = in.readUTF();
+					System.out.println("Arquivo: " + nomeDir);
+					File arquivoPraDelecao = new File(nomeDir);
+					if (arquivoPraDelecao.isFile()) {
+						arquivoPraDelecao.delete();
+						System.out.println("Arquivo deletado");
+					} else
+						System.out.println("Arquivo não existe");
+
+					dos = new DataOutputStream(ser.getOutputStream());
+					novaLista = visualizarArquivos();
+					dos.writeUTF(novaLista);
+					dos.flush();
+					server.close();
+					break;
+				case "deletarDiretorio":
+					System.out.println("recebendo diretorio para deleção...");
+					System.out.println();
+					in = new DataInputStream(ser.getInputStream());
+					nomeDir = in.readUTF();
+					System.out.println("Diretorio: " + nomeDir);
+					server.close();
+					File dirToDelete = new File(nomeDir);
+					if (dirToDelete.isFile()) {
+						deleteArquivos(dirToDelete);
+						System.out.println("Diretorio deletado");
+					} else
+						System.out.println("Diretorio não existe");
+					dos = new DataOutputStream(ser.getOutputStream());
+					novaLista = visualizarArquivos();
+					dos.writeUTF(novaLista);
+					dos.flush();
+					server.close();
+					break;
+
+				case "enviarArquivo":
+					System.out.println("recebendo nome do arquivo...");
+					in = new DataInputStream(ser.getInputStream());
+					String nomeArquivo = in.readUTF();
+					System.out.println(nomeArquivo);
+
+					System.out.println("recebendo diretorio do arquivo...");
+
+					in = new DataInputStream(ser.getInputStream());
+					String dir = in.readUTF();
+					System.out.println(dir);
+
+					ObjectInputStream input = new ObjectInputStream(ser.getInputStream());
+
+					FileOutputStream file;
+					file = new FileOutputStream(dir + nomeArquivo);
+
+					byte[] buffer = new byte[4096];
+					System.out.println("recebendo arquivo...");
+					while (true) {
+						int tamanho = input.read(buffer);
+						if (tamanho == -1)
+							break;
+						file.write(buffer, 0, tamanho);
+					}
+
+					DataOutputStream out = new DataOutputStream(ser.getOutputStream());
+					out.writeUTF("sua mensagem foi recebida");// retornando mensagem
+					server.close();
+					break;
+
+				}
+//			//visualizarArquivos();
+//			Server server = new Server();
+//			System.out.println("Aguardadndo conexao");
+//			
+//			server.criarServerSocket(1125);
+//			System.out.println(" cliente conectado");
+//			while (true) {
+//				Socket socket = server.esperaConexao();
+//				server.trataConexao(socket);
+//				System.out.println("cliente finalizado");
+//			}
+
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
